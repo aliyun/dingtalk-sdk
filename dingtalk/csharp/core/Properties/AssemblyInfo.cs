f7574a0600b67d1690ca668f97f5ffa2a0960447
sdk-team <sdk-team@alibabacloud.com>
2021-03-01 07:21:55 +0000

    Online.

--- dingtalk/csharp/core/Properties/AssemblyInfo.cs
+++ dingtalk/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("74c68600-7801-11eb-838c-adc4d4eedc9f")]
+[assembly: Guid("8d3e0aa0-7a5e-11eb-8ba8-5921dda86cfb")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ dingtalk/csharp/core/hrbrain_1_0/Models/SyncDataRequest.cs
@@ -0,0 +1,34 @@
+// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+namespace AlibabaCloud.SDK.Dingtalkhrbrain_1_0.Models
+{
+    public class SyncDataRequest : TeaModel {
+        [NameInMap("projectId")]
+        [Validation(Required=false)]
+        public string ProjectId { get; set; }
+
+        [NameInMap("schemaId")]
+        [Validation(Required=false)]
+        public string SchemaId { get; set; }
+
+        [NameInMap("dataId")]
+        [Validation(Required=false)]
+        public string DataId { get; set; }
+
+        [NameInMap("content")]
+        [Validation(Required=false)]
+        public string Content { get; set; }
+
+        [NameInMap("etlTime")]
+        [Validation(Required=false)]
+        public string EtlTime { get; set; }
+
+    }
+
+}
