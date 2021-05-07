9ffffc04d67beb2fda598ac93b4f595940219083
sdk-team <sdk-team@alibabacloud.com>
2021-05-07 03:37:12 +0000

    Online.

--- dingtalk/python/alibabacloud_dingtalk/workbench_1_0/client.py
+++ dingtalk/python/alibabacloud_dingtalk/workbench_1_0/client.py
@@ -7,6 +7,7 @@ from alibabacloud_tea_openapi import models as open_api_models
 from alibabacloud_tea_util.client import Client as UtilClient
 from alibabacloud_dingtalkworkbench_1_0 import models as dingtalkworkbench__1__0_models
 from alibabacloud_tea_util import models as util_models
+from alibabacloud_openapi_util.client import Client as OpenApiUtilClient
 
 
 class Client(OpenApiClient):
@@ -21,6 +22,144 @@ class Client(OpenApiClient):
         self._endpoint_rule = ''
         if UtilClient.empty(self._endpoint):
             self._endpoint = 'api.dingtalk.com'
+
+    def query_component_scopes(
+        self,
+        component_id: str,
+    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+        runtime = util_models.RuntimeOptions()
+        headers = dingtalkworkbench__1__0_models.QueryComponentScopesHeaders()
+        return self.query_component_scopes_with_options(component_id, headers, runtime)
+
+    async def query_component_scopes_async(
+        self,
+        component_id: str,
+    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+        runtime = util_models.RuntimeOptions()
+        headers = dingtalkworkbench__1__0_models.QueryComponentScopesHeaders()
+        return await self.query_component_scopes_with_options_async(component_id, headers, runtime)
+
+    def query_component_scopes_with_options(
+        self,
+        component_id: str,
+        headers: dingtalkworkbench__1__0_models.QueryComponentScopesHeaders,
+        runtime: util_models.RuntimeOptions,
+    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+        real_headers = {}
+        if not UtilClient.is_unset(headers.common_headers):
+            real_headers = headers.common_headers
+        if not UtilClient.is_unset(headers.x_acs_dingtalk_access_token):
+            real_headers['x-acs-dingtalk-access-token'] = headers.x_acs_dingtalk_access_token
+        req = open_api_models.OpenApiRequest(
+            headers=real_headers
+        )
+        return TeaCore.from_map(
+            dingtalkworkbench__1__0_models.QueryComponentScopesResponse(),
+            self.do_roarequest('QueryComponentScopes', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/components/{component_id}/scopes', 'json', req, runtime)
+        )
+
+    async def query_component_scopes_with_options_async(
+        self,
+        component_id: str,
+        headers: dingtalkworkbench__1__0_models.QueryComponentScopesHeaders,
+        runtime: util_models.RuntimeOptions,
+    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+        real_headers = {}
+        if not UtilClient.is_unset(headers.common_headers):
+            real_headers = headers.common_headers
+        if not UtilClient.is_unset(headers.x_acs_dingtalk_access_token):
+            real_headers['x-acs-dingtalk-access-token'] = headers.x_acs_dingtalk_access_token
+        req = open_api_models.OpenApiRequest(
+            headers=real_headers
+        )
+        return TeaCore.from_map(
+            dingtalkworkbench__1__0_models.QueryComponentScopesResponse(),
+            await self.do_roarequest_async('QueryComponentScopes', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/components/{component_id}/scopes', 'json', req, runtime)
+        )
+
+    def update_ding_portal_page_scope(
+        self,
+        page_uuid: str,
+        app_uuid: str,
+        request: dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeRequest,
+    ) -> dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeResponse:
+        runtime = util_models.RuntimeOptions()
+        headers = dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeHeaders()
+        return self.update_ding_portal_page_scope_with_options(page_uuid, app_uuid, request, headers, runtime)
+
+    async def update_ding_portal_page_scope_async(
+        self,
+        page_uuid: str,
+        app_uuid: str,
+        request: dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeRequest,
+    ) -> dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeResponse:
+        runtime = util_models.RuntimeOptions()
+        headers = dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeHeaders()
+        return await self.update_ding_portal_page_scope_with_options_async(page_uuid, app_uuid, request, headers, runtime)
+
+    def update_ding_portal_page_scope_with_options(
+        self,
+        page_uuid: str,
+        app_uuid: str,
+        request: dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeRequest,
+        headers: dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeHeaders,
+        runtime: util_models.RuntimeOptions,
+    ) -> dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeResponse:
+        UtilClient.validate_model(request)
+        body = {}
+        if not UtilClient.is_unset(request.userids):
+            body['userids'] = request.userids
+        if not UtilClient.is_unset(request.dept_ids):
+            body['deptIds'] = request.dept_ids
+        if not UtilClient.is_unset(request.role_ids):
+            body['roleIds'] = request.role_ids
+        if not UtilClient.is_unset(request.all_visible):
+            body['allVisible'] = request.all_visible
+        real_headers = {}
+        if not UtilClient.is_unset(headers.common_headers):
+            real_headers = headers.common_headers
+        if not UtilClient.is_unset(headers.x_acs_dingtalk_access_token):
+            real_headers['x-acs-dingtalk-access-token'] = headers.x_acs_dingtalk_access_token
+        req = open_api_models.OpenApiRequest(
+            headers=real_headers,
+            body=OpenApiUtilClient.parse_to_map(body)
+        )
+        return TeaCore.from_map(
+            dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeResponse(),
+            self.do_roarequest('UpdateDingPortalPageScope', 'workbench_1.0', 'HTTP', 'PUT', 'AK', f'/v1.0/workbench/dingPortals/{app_uuid}/pageScopes/{page_uuid}', 'none', req, runtime)
+        )
+
+    async def update_ding_portal_page_scope_with_options_async(
+        self,
+        page_uuid: str,
+        app_uuid: str,
+        request: dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeRequest,
+        headers: dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeHeaders,
+        runtime: util_models.RuntimeOptions,
+    ) -> dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeResponse:
+        UtilClient.validate_model(request)
+        body = {}
+        if not UtilClient.is_unset(request.userids):
+            body['userids'] = request.userids
+        if not UtilClient.is_unset(request.dept_ids):
+            body['deptIds'] = request.dept_ids
+        if not UtilClient.is_unset(request.role_ids):
+            body['roleIds'] = request.role_ids
+        if not UtilClient.is_unset(request.all_visible):
+            body['allVisible'] = request.all_visible
+        real_headers = {}
+        if not UtilClient.is_unset(headers.common_headers):
+            real_headers = headers.common_headers
+        if not UtilClient.is_unset(headers.x_acs_dingtalk_access_token):
+            real_headers['x-acs-dingtalk-access-token'] = headers.x_acs_dingtalk_access_token
+        req = open_api_models.OpenApiRequest(
+            headers=real_headers,
+            body=OpenApiUtilClient.parse_to_map(body)
+        )
+        return TeaCore.from_map(
+            dingtalkworkbench__1__0_models.UpdateDingPortalPageScopeResponse(),
+            await self.do_roarequest_async('UpdateDingPortalPageScope', 'workbench_1.0', 'HTTP', 'PUT', 'AK', f'/v1.0/workbench/dingPortals/{app_uuid}/pageScopes/{page_uuid}', 'none', req, runtime)
+        )
 
     def query_shortcut_scopes(
         self,
@@ -76,28 +215,28 @@ class Client(OpenApiClient):
             await self.do_roarequest_async('QueryShortcutScopes', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/shortcuts/{shortcut_key}/scopes', 'json', req, runtime)
         )
 
-    def query_component_scopes(
+    def get_ding_portal_detail(
         self,
-        component_id: str,
-    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+        app_uuid: str,
+    ) -> dingtalkworkbench__1__0_models.GetDingPortalDetailResponse:
         runtime = util_models.RuntimeOptions()
-        headers = dingtalkworkbench__1__0_models.QueryComponentScopesHeaders()
-        return self.query_component_scopes_with_options(component_id, headers, runtime)
+        headers = dingtalkworkbench__1__0_models.GetDingPortalDetailHeaders()
+        return self.get_ding_portal_detail_with_options(app_uuid, headers, runtime)
 
-    async def query_component_scopes_async(
+    async def get_ding_portal_detail_async(
         self,
-        component_id: str,
-    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+        app_uuid: str,
+    ) -> dingtalkworkbench__1__0_models.GetDingPortalDetailResponse:
         runtime = util_models.RuntimeOptions()
-        headers = dingtalkworkbench__1__0_models.QueryComponentScopesHeaders()
-        return await self.query_component_scopes_with_options_async(component_id, headers, runtime)
+        headers = dingtalkworkbench__1__0_models.GetDingPortalDetailHeaders()
+        return await self.get_ding_portal_detail_with_options_async(app_uuid, headers, runtime)
 
-    def query_component_scopes_with_options(
+    def get_ding_portal_detail_with_options(
         self,
-        component_id: str,
-        headers: dingtalkworkbench__1__0_models.QueryComponentScopesHeaders,
+        app_uuid: str,
+        headers: dingtalkworkbench__1__0_models.GetDingPortalDetailHeaders,
         runtime: util_models.RuntimeOptions,
-    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+    ) -> dingtalkworkbench__1__0_models.GetDingPortalDetailResponse:
         real_headers = {}
         if not UtilClient.is_unset(headers.common_headers):
             real_headers = headers.common_headers
@@ -107,16 +246,16 @@ class Client(OpenApiClient):
             headers=real_headers
         )
         return TeaCore.from_map(
-            dingtalkworkbench__1__0_models.QueryComponentScopesResponse(),
-            self.do_roarequest('QueryComponentScopes', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/components/{component_id}/scopes', 'json', req, runtime)
+            dingtalkworkbench__1__0_models.GetDingPortalDetailResponse(),
+            self.do_roarequest('GetDingPortalDetail', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/dingPortals/{app_uuid}', 'json', req, runtime)
         )
 
-    async def query_component_scopes_with_options_async(
+    async def get_ding_portal_detail_with_options_async(
         self,
-        component_id: str,
-        headers: dingtalkworkbench__1__0_models.QueryComponentScopesHeaders,
+        app_uuid: str,
+        headers: dingtalkworkbench__1__0_models.GetDingPortalDetailHeaders,
         runtime: util_models.RuntimeOptions,
-    ) -> dingtalkworkbench__1__0_models.QueryComponentScopesResponse:
+    ) -> dingtalkworkbench__1__0_models.GetDingPortalDetailResponse:
         real_headers = {}
         if not UtilClient.is_unset(headers.common_headers):
             real_headers = headers.common_headers
@@ -126,6 +265,6 @@ class Client(OpenApiClient):
             headers=real_headers
         )
         return TeaCore.from_map(
-            dingtalkworkbench__1__0_models.QueryComponentScopesResponse(),
-            await self.do_roarequest_async('QueryComponentScopes', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/components/{component_id}/scopes', 'json', req, runtime)
+            dingtalkworkbench__1__0_models.GetDingPortalDetailResponse(),
+            await self.do_roarequest_async('GetDingPortalDetail', 'workbench_1.0', 'HTTP', 'GET', 'AK', f'/v1.0/workbench/dingPortals/{app_uuid}', 'json', req, runtime)
         )
--- dingtalk/python/alibabacloud_dingtalk/workbench_1_0/models.py
+++ dingtalk/python/alibabacloud_dingtalk/workbench_1_0/models.py
@@ -4,6 +4,219 @@ from Tea.model import TeaModel
 from typing import Dict, List
 
 
+class QueryComponentScopesHeaders(TeaModel):
+    def __init__(
+        self,
+        common_headers: Dict[str, str] = None,
+        x_acs_dingtalk_access_token: str = None,
+    ):
+        self.common_headers = common_headers
+        self.x_acs_dingtalk_access_token = x_acs_dingtalk_access_token
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.common_headers is not None:
+            result['commonHeaders'] = self.common_headers
+        if self.x_acs_dingtalk_access_token is not None:
+            result['x-acs-dingtalk-access-token'] = self.x_acs_dingtalk_access_token
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('commonHeaders') is not None:
+            self.common_headers = m.get('commonHeaders')
+        if m.get('x-acs-dingtalk-access-token') is not None:
+            self.x_acs_dingtalk_access_token = m.get('x-acs-dingtalk-access-token')
+        return self
+
+
+class QueryComponentScopesResponseBody(TeaModel):
+    def __init__(
+        self,
+        user_visible_scopes: List[str] = None,
+        dept_visible_scopes: List[int] = None,
+    ):
+        # scopes
+        self.user_visible_scopes = user_visible_scopes
+        self.dept_visible_scopes = dept_visible_scopes
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.user_visible_scopes is not None:
+            result['userVisibleScopes'] = self.user_visible_scopes
+        if self.dept_visible_scopes is not None:
+            result['deptVisibleScopes'] = self.dept_visible_scopes
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('userVisibleScopes') is not None:
+            self.user_visible_scopes = m.get('userVisibleScopes')
+        if m.get('deptVisibleScopes') is not None:
+            self.dept_visible_scopes = m.get('deptVisibleScopes')
+        return self
+
+
+class QueryComponentScopesResponse(TeaModel):
+    def __init__(
+        self,
+        headers: Dict[str, str] = None,
+        body: QueryComponentScopesResponseBody = None,
+    ):
+        self.headers = headers
+        self.body = body
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+        self.validate_required(self.body, 'body')
+        if self.body:
+            self.body.validate()
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        if self.body is not None:
+            result['body'] = self.body.to_map()
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        if m.get('body') is not None:
+            temp_model = QueryComponentScopesResponseBody()
+            self.body = temp_model.from_map(m['body'])
+        return self
+
+
+class UpdateDingPortalPageScopeHeaders(TeaModel):
+    def __init__(
+        self,
+        common_headers: Dict[str, str] = None,
+        x_acs_dingtalk_access_token: str = None,
+    ):
+        self.common_headers = common_headers
+        self.x_acs_dingtalk_access_token = x_acs_dingtalk_access_token
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.common_headers is not None:
+            result['commonHeaders'] = self.common_headers
+        if self.x_acs_dingtalk_access_token is not None:
+            result['x-acs-dingtalk-access-token'] = self.x_acs_dingtalk_access_token
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('commonHeaders') is not None:
+            self.common_headers = m.get('commonHeaders')
+        if m.get('x-acs-dingtalk-access-token') is not None:
+            self.x_acs_dingtalk_access_token = m.get('x-acs-dingtalk-access-token')
+        return self
+
+
+class UpdateDingPortalPageScopeRequest(TeaModel):
+    def __init__(
+        self,
+        userids: List[str] = None,
+        dept_ids: List[int] = None,
+        role_ids: List[int] = None,
+        all_visible: bool = None,
+    ):
+        # 可见用户列表
+        self.userids = userids
+        # 可见部门列表
+        self.dept_ids = dept_ids
+        # 可见角色列表
+        self.role_ids = role_ids
+        # 是否全员可见
+        self.all_visible = all_visible
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.userids is not None:
+            result['userids'] = self.userids
+        if self.dept_ids is not None:
+            result['deptIds'] = self.dept_ids
+        if self.role_ids is not None:
+            result['roleIds'] = self.role_ids
+        if self.all_visible is not None:
+            result['allVisible'] = self.all_visible
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('userids') is not None:
+            self.userids = m.get('userids')
+        if m.get('deptIds') is not None:
+            self.dept_ids = m.get('deptIds')
+        if m.get('roleIds') is not None:
+            self.role_ids = m.get('roleIds')
+        if m.get('allVisible') is not None:
+            self.all_visible = m.get('allVisible')
+        return self
+
+
+class UpdateDingPortalPageScopeResponse(TeaModel):
+    def __init__(
+        self,
+        headers: Dict[str, str] = None,
+    ):
+        self.headers = headers
+
+    def validate(self):
+        self.validate_required(self.headers, 'headers')
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.headers is not None:
+            result['headers'] = self.headers
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('headers') is not None:
+            self.headers = m.get('headers')
+        return self
+
+
 class QueryShortcutScopesHeaders(TeaModel):
     def __init__(
         self,
@@ -108,7 +321,7 @@ class QueryShortcutScopesResponse(TeaModel):
         return self
 
 
-class QueryComponentScopesHeaders(TeaModel):
+class GetDingPortalDetailHeaders(TeaModel):
     def __init__(
         self,
         common_headers: Dict[str, str] = None,
@@ -141,15 +354,28 @@ class QueryComponentScopesHeaders(TeaModel):
         return self
 
 
-class QueryComponentScopesResponseBody(TeaModel):
+class GetDingPortalDetailResponseBodyPages(TeaModel):
     def __init__(
         self,
-        user_visible_scopes: List[str] = None,
-        dept_visible_scopes: List[int] = None,
+        page_uuid: str = None,
+        page_name: str = None,
+        userids: List[str] = None,
+        dept_ids: List[int] = None,
+        role_ids: List[int] = None,
+        all_visible: bool = None,
     ):
-        # scopes
-        self.user_visible_scopes = user_visible_scopes
-        self.dept_visible_scopes = dept_visible_scopes
+        # 页面ID
+        self.page_uuid = page_uuid
+        # 页面名称
+        self.page_name = page_name
+        # 可见员工 ID 列表
+        self.userids = userids
+        # 可见部门 ID 铺
+        self.dept_ids = dept_ids
+        # 可见角色列表
+        self.role_ids = role_ids
+        # 是否全公司可见
+        self.all_visible = all_visible
 
     def validate(self):
         pass
@@ -160,26 +386,92 @@ class QueryComponentScopesResponseBody(TeaModel):
             return _map
 
         result = dict()
-        if self.user_visible_scopes is not None:
-            result['userVisibleScopes'] = self.user_visible_scopes
-        if self.dept_visible_scopes is not None:
-            result['deptVisibleScopes'] = self.dept_visible_scopes
+        if self.page_uuid is not None:
+            result['pageUuid'] = self.page_uuid
+        if self.page_name is not None:
+            result['pageName'] = self.page_name
+        if self.userids is not None:
+            result['userids'] = self.userids
+        if self.dept_ids is not None:
+            result['deptIds'] = self.dept_ids
+        if self.role_ids is not None:
+            result['roleIds'] = self.role_ids
+        if self.all_visible is not None:
+            result['allVisible'] = self.all_visible
         return result
 
     def from_map(self, m: dict = None):
         m = m or dict()
-        if m.get('userVisibleScopes') is not None:
-            self.user_visible_scopes = m.get('userVisibleScopes')
-        if m.get('deptVisibleScopes') is not None:
-            self.dept_visible_scopes = m.get('deptVisibleScopes')
+        if m.get('pageUuid') is not None:
+            self.page_uuid = m.get('pageUuid')
+        if m.get('pageName') is not None:
+            self.page_name = m.get('pageName')
+        if m.get('userids') is not None:
+            self.userids = m.get('userids')
+        if m.get('deptIds') is not None:
+            self.dept_ids = m.get('deptIds')
+        if m.get('roleIds') is not None:
+            self.role_ids = m.get('roleIds')
+        if m.get('allVisible') is not None:
+            self.all_visible = m.get('allVisible')
         return self
 
 
-class QueryComponentScopesResponse(TeaModel):
+class GetDingPortalDetailResponseBody(TeaModel):
+    def __init__(
+        self,
+        app_uuid: str = None,
+        ding_portal_name: str = None,
+        pages: List[GetDingPortalDetailResponseBodyPages] = None,
+    ):
+        # 工作台ID
+        self.app_uuid = app_uuid
+        # 工作台名称
+        self.ding_portal_name = ding_portal_name
+        # 工作台页面信息
+        self.pages = pages
+
+    def validate(self):
+        if self.pages:
+            for k in self.pages:
+                if k:
+                    k.validate()
+
+    def to_map(self):
+        _map = super().to_map()
+        if _map is not None:
+            return _map
+
+        result = dict()
+        if self.app_uuid is not None:
+            result['appUuid'] = self.app_uuid
+        if self.ding_portal_name is not None:
+            result['dingPortalName'] = self.ding_portal_name
+        result['pages'] = []
+        if self.pages is not None:
+            for k in self.pages:
+                result['pages'].append(k.to_map() if k else None)
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('appUuid') is not None:
+            self.app_uuid = m.get('appUuid')
+        if m.get('dingPortalName') is not None:
+            self.ding_portal_name = m.get('dingPortalName')
+        self.pages = []
+        if m.get('pages') is not None:
+            for k in m.get('pages'):
+                temp_model = GetDingPortalDetailResponseBodyPages()
+                self.pages.append(temp_model.from_map(k))
+        return self
+
+
+class GetDingPortalDetailResponse(TeaModel):
     def __init__(
         self,
         headers: Dict[str, str] = None,
-        body: QueryComponentScopesResponseBody = None,
+        body: GetDingPortalDetailResponseBody = None,
     ):
         self.headers = headers
         self.body = body
@@ -207,7 +499,7 @@ class QueryComponentScopesResponse(TeaModel):
         if m.get('headers') is not None:
             self.headers = m.get('headers')
         if m.get('body') is not None:
-            temp_model = QueryComponentScopesResponseBody()
+            temp_model = GetDingPortalDetailResponseBody()
             self.body = temp_model.from_map(m['body'])
         return self
 
