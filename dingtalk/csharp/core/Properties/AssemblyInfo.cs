437d70081a6be81c95cc7e15c2b8f10ad9749954
sdk-team <sdk-team@alibabacloud.com>
2021-05-17 08:29:52 +0000

    Online.

--- dingtalk/csharp/core/Properties/AssemblyInfo.cs
+++ dingtalk/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88331c90-b6bc-11eb-ac6f-11cf4f857f7d")]
+[assembly: Guid("6bce9cf0-b6e9-11eb-a06c-85a07f2a6ba3")]
 
 // Version information for an assembly consists of the following four values:
 //
