d6e2718e9c578b2b3b836e6e82784873e1e902a8
sdk-team <sdk-team@alibabacloud.com>
2021-10-20 01:02:19 +0000

    Online.

--- dingtalk/java/src/main/java/com/aliyun/dingtalkdingmi_1_0/Client.java
+++ dingtalk/java/src/main/java/com/aliyun/dingtalkdingmi_1_0/Client.java
@@ -386,11 +386,11 @@ public class Client extends com.aliyun.teaopenapi.Client {
 
     public AddRobotInstanceToGroupResponse addRobotInstanceToGroup(AddRobotInstanceToGroupRequest request) throws Exception {
         RuntimeOptions runtime = new RuntimeOptions();
-        java.util.Map<String, String> headers = new java.util.HashMap<>();
+        AddRobotInstanceToGroupHeaders headers = new AddRobotInstanceToGroupHeaders();
         return this.addRobotInstanceToGroupWithOptions(request, headers, runtime);
     }
 
-    public AddRobotInstanceToGroupResponse addRobotInstanceToGroupWithOptions(AddRobotInstanceToGroupRequest request, java.util.Map<String, String> headers, RuntimeOptions runtime) throws Exception {
+    public AddRobotInstanceToGroupResponse addRobotInstanceToGroupWithOptions(AddRobotInstanceToGroupRequest request, AddRobotInstanceToGroupHeaders headers, RuntimeOptions runtime) throws Exception {
         com.aliyun.teautil.Common.validateModel(request);
         java.util.Map<String, Object> body = new java.util.HashMap<>();
         if (!com.aliyun.teautil.Common.isUnset(request.dingCorpId)) {
@@ -405,8 +405,17 @@ public class Client extends com.aliyun.teaopenapi.Client {
             body.put("openConversationId", request.openConversationId);
         }
 
+        java.util.Map<String, String> realHeaders = new java.util.HashMap<>();
+        if (!com.aliyun.teautil.Common.isUnset(headers.commonHeaders)) {
+            realHeaders = headers.commonHeaders;
+        }
+
+        if (!com.aliyun.teautil.Common.isUnset(headers.xAcsDingtalkAccessToken)) {
+            realHeaders.put("x-acs-dingtalk-access-token", headers.xAcsDingtalkAccessToken);
+        }
+
         OpenApiRequest req = OpenApiRequest.build(TeaConverter.buildMap(
-            new TeaPair("headers", headers),
+            new TeaPair("headers", realHeaders),
             new TeaPair("body", com.aliyun.openapiutil.Client.parseToMap(body))
         ));
         return TeaModel.toModel(this.doROARequest("AddRobotInstanceToGroup", "dingmi_1.0", "HTTP", "POST", "AK", "/v1.0/dingmi/intelligentRobots/groups", "json", req, runtime), new AddRobotInstanceToGroupResponse());
