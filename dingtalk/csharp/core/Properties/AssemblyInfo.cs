d28801f1138643c1cea316620c4799afa44f550b
sdk-team <sdk-team@alibabacloud.com>
2022-10-26 02:49:34 +0000

    Update sync interface.

--- dingtalk/csharp/core/Properties/AssemblyInfo.cs
+++ dingtalk/csharp/core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("486b9cf0-5412-11ed-9702-eb4b64e69cc7")]
+[assembly: Guid("4d164e50-54d8-11ed-b6d3-53583fbf1fa4")]
 
 // Version information for an assembly consists of the following four values:
 //
