2470f1c916a5529abb759ecf292274202517a874
sdk-team <sdk-team@alibabacloud.com>
2023-07-19 03:10:45 +0000

    Update AddOfficialAccountFollower.

--- dingtalk/ts/src/robot_1_0/client.ts
+++ dingtalk/ts/src/robot_1_0/client.ts
@@ -564,6 +564,100 @@ export class ClearRobotPluginResponse extends $tea.Model {
   }
 }
 
+export class ExecuteRobotAiSkillHeaders extends $tea.Model {
+  commonHeaders?: { [key: string]: string };
+  xAcsDingtalkAccessToken?: string;
+  static names(): { [key: string]: string } {
+    return {
+      commonHeaders: 'commonHeaders',
+      xAcsDingtalkAccessToken: 'x-acs-dingtalk-access-token',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      commonHeaders: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      xAcsDingtalkAccessToken: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class ExecuteRobotAiSkillRequest extends $tea.Model {
+  context?: { [key: string]: any };
+  input?: string;
+  robotCode?: string;
+  skillId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      context: 'context',
+      input: 'input',
+      robotCode: 'robotCode',
+      skillId: 'skillId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      context: { 'type': 'map', 'keyType': 'string', 'valueType': 'any' },
+      input: 'string',
+      robotCode: 'string',
+      skillId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class ExecuteRobotAiSkillResponseBody extends $tea.Model {
+  result?: string;
+  static names(): { [key: string]: string } {
+    return {
+      result: 'result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      result: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class ExecuteRobotAiSkillResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: ExecuteRobotAiSkillResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: ExecuteRobotAiSkillResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class GetBotListInGroupHeaders extends $tea.Model {
   commonHeaders?: { [key: string]: string };
   xAcsDingtalkAccessToken?: string;
@@ -2382,6 +2476,58 @@ export default class Client extends OpenApi {
     let runtime = new $Util.RuntimeOptions({ });
     let headers = new ClearRobotPluginHeaders({ });
     return await this.clearRobotPluginWithOptions(request, headers, runtime);
+  }
+
+  async executeRobotAiSkillWithOptions(request: ExecuteRobotAiSkillRequest, headers: ExecuteRobotAiSkillHeaders, runtime: $Util.RuntimeOptions): Promise<ExecuteRobotAiSkillResponse> {
+    Util.validateModel(request);
+    let body : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.context)) {
+      body["context"] = request.context;
+    }
+
+    if (!Util.isUnset(request.input)) {
+      body["input"] = request.input;
+    }
+
+    if (!Util.isUnset(request.robotCode)) {
+      body["robotCode"] = request.robotCode;
+    }
+
+    if (!Util.isUnset(request.skillId)) {
+      body["skillId"] = request.skillId;
+    }
+
+    let realHeaders : {[key: string ]: string} = { };
+    if (!Util.isUnset(headers.commonHeaders)) {
+      realHeaders = headers.commonHeaders;
+    }
+
+    if (!Util.isUnset(headers.xAcsDingtalkAccessToken)) {
+      realHeaders["x-acs-dingtalk-access-token"] = Util.toJSONString(headers.xAcsDingtalkAccessToken);
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      headers: realHeaders,
+      body: OpenApiUtil.parseToMap(body),
+    });
+    let params = new $OpenApi.Params({
+      action: "ExecuteRobotAiSkill",
+      version: "robot_1.0",
+      protocol: "HTTP",
+      pathname: `/v1.0/robot/aiSkill/execute`,
+      method: "POST",
+      authType: "AK",
+      style: "ROA",
+      reqBodyType: "none",
+      bodyType: "json",
+    });
+    return $tea.cast<ExecuteRobotAiSkillResponse>(await this.execute(params, req, runtime), new ExecuteRobotAiSkillResponse({}));
+  }
+
+  async executeRobotAiSkill(request: ExecuteRobotAiSkillRequest): Promise<ExecuteRobotAiSkillResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers = new ExecuteRobotAiSkillHeaders({ });
+    return await this.executeRobotAiSkillWithOptions(request, headers, runtime);
   }
 
   async getBotListInGroupWithOptions(request: GetBotListInGroupRequest, headers: GetBotListInGroupHeaders, runtime: $Util.RuntimeOptions): Promise<GetBotListInGroupResponse> {
