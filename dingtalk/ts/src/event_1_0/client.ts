e1d64053ddd76d82ae7ddc95772c957354cd8a7e
sdk-team <sdk-team@alibabacloud.com>
2023-11-04 04:12:39 +0000

    Generated ts activity_1.0,aiPaaS_1.0,algo_1.0,alitest_1.0,alitrip_1.0,apaas_1.0,appMarket_1.0,ats_1.0,attendance_1.0,badge_1.0,bayMax_1.0,baymax_2.0,bipaas_1.0,bizfinance_1.0,bizfinance_2.0,blackboard_1.0,calendar_1.0,calendar_2.0,carbon_1.0,card_1.0,checkIn_1.0,chengfeng_1.0,conference_1.0,connector_1.0,contact_1.0,content_1.0,contract_1.0,convFile_1.0,convStorage_1.0,coolOps_1.0,credit_1.0,crm_1.0,crm_2.0,customerService_1.0,datacenter_1.0,delivery_1.0,devicemng_1.0,dingmi_1.0,dingPhone_1.0,dingsport_1.0,diot_1.0,doc_1.0,doc_2.0,drive_1.0,drive_2.0,edu_1.0,esign_1.0,event_1.0,event_2.0,exclusive_1.0,finance_1.0,flashmeeting_1.0,flashmsg_1.0,gateway_1.0,groupBlackboard_1.0,h5package_1.0,hrbrain_1.0,hrm_1.0,im_1.0,im_2.0,impaas_1.0,industry_1.0,integration_1.0,liandanlu_1.0,link_1.0,live_1.0,liveActivities_1.0,mail_1.0,manufacturing_1.0,media_1.0,medical_1.0,microApp_1.0,miniapp_1.0,oauth2_1.0,occupationauth_1.0,office_1.0,okr_1.0,orgCulture_1.0,ow_1.0,package_1.0,pedia_1.0,project_1.0,projectIntegration_1.0,rcsCall_1.0,report_1.0,resident_1.0,robot_1.0,rooms_1.0,search_1.0,serviceGroup_1.0,setting_1.0,smartDevice_1.0,snsStorage_1.0,storage_1.0,storage_2.0,swform_1.0,theone_1.0,todo_1.0,trade_1.0,trajectory_1.0,transcribe_1.0,trip_1.0,village_1.0,waterMark_1.0,watt_1.0,wiki_1.0,wiki_2.0,wms_1.0,workbench_1.0,workflow_1.0,workrecord_1.0,yida_1.0,yunShu_1.0,esign_1.1,esign_2.0,h3yun_1.0,jzcrm_1.0,soke_1.0 for dingtalk.

--- dingtalk/ts/src/event_1_0/client.ts
+++ dingtalk/ts/src/event_1_0/client.ts
@@ -100,6 +100,246 @@ export class GetCallBackFaileResultResponse extends $tea.Model {
   }
 }
 
+export class InstallAppHeaders extends $tea.Model {
+  commonHeaders?: { [key: string]: string };
+  xAcsDingtalkAccessToken?: string;
+  static names(): { [key: string]: string } {
+    return {
+      commonHeaders: 'commonHeaders',
+      xAcsDingtalkAccessToken: 'x-acs-dingtalk-access-token',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      commonHeaders: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      xAcsDingtalkAccessToken: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallAppRequest extends $tea.Model {
+  appId?: number;
+  corpId?: string;
+  dingUserId?: string;
+  suiteId?: number;
+  static names(): { [key: string]: string } {
+    return {
+      appId: 'appId',
+      corpId: 'corpId',
+      dingUserId: 'dingUserId',
+      suiteId: 'suiteId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      appId: 'number',
+      corpId: 'string',
+      dingUserId: 'string',
+      suiteId: 'number',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallAppResponseBody extends $tea.Model {
+  result?: boolean;
+  static names(): { [key: string]: string } {
+    return {
+      result: 'result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      result: 'boolean',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallAppResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: InstallAppResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: InstallAppResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallCoolAppHeaders extends $tea.Model {
+  commonHeaders?: { [key: string]: string };
+  xAcsDingtalkAccessToken?: string;
+  static names(): { [key: string]: string } {
+    return {
+      commonHeaders: 'commonHeaders',
+      xAcsDingtalkAccessToken: 'x-acs-dingtalk-access-token',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      commonHeaders: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      xAcsDingtalkAccessToken: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallCoolAppRequest extends $tea.Model {
+  appId?: number;
+  coolAppCode?: string;
+  corpId?: string;
+  feature?: { [key: string]: any };
+  installUid?: string;
+  openConversationId?: string;
+  options?: { [key: string]: any };
+  suiteId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      appId: 'appId',
+      coolAppCode: 'coolAppCode',
+      corpId: 'corpId',
+      feature: 'feature',
+      installUid: 'installUid',
+      openConversationId: 'openConversationId',
+      options: 'options',
+      suiteId: 'suiteId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      appId: 'number',
+      coolAppCode: 'string',
+      corpId: 'string',
+      feature: { 'type': 'map', 'keyType': 'string', 'valueType': 'any' },
+      installUid: 'string',
+      openConversationId: 'string',
+      options: { 'type': 'map', 'keyType': 'string', 'valueType': 'any' },
+      suiteId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallCoolAppShrinkRequest extends $tea.Model {
+  appId?: number;
+  coolAppCode?: string;
+  corpId?: string;
+  featureShrink?: string;
+  installUid?: string;
+  openConversationId?: string;
+  optionsShrink?: string;
+  suiteId?: string;
+  static names(): { [key: string]: string } {
+    return {
+      appId: 'appId',
+      coolAppCode: 'coolAppCode',
+      corpId: 'corpId',
+      featureShrink: 'feature',
+      installUid: 'installUid',
+      openConversationId: 'openConversationId',
+      optionsShrink: 'options',
+      suiteId: 'suiteId',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      appId: 'number',
+      coolAppCode: 'string',
+      corpId: 'string',
+      featureShrink: 'string',
+      installUid: 'string',
+      openConversationId: 'string',
+      optionsShrink: 'string',
+      suiteId: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallCoolAppResponseBody extends $tea.Model {
+  result?: string;
+  static names(): { [key: string]: string } {
+    return {
+      result: 'result',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      result: 'string',
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
+export class InstallCoolAppResponse extends $tea.Model {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body: InstallCoolAppResponseBody;
+  static names(): { [key: string]: string } {
+    return {
+      headers: 'headers',
+      statusCode: 'statusCode',
+      body: 'body',
+    };
+  }
+
+  static types(): { [key: string]: any } {
+    return {
+      headers: { 'type': 'map', 'keyType': 'string', 'valueType': 'string' },
+      statusCode: 'number',
+      body: InstallCoolAppResponseBody,
+    };
+  }
+
+  constructor(map?: { [key: string]: any }) {
+    super(map);
+  }
+}
+
 export class RePushSuiteTicketRequest extends $tea.Model {
   suiteId?: number;
   suiteSecret?: string;
@@ -253,6 +493,136 @@ export default class Client extends OpenApi {
     let runtime = new $Util.RuntimeOptions({ });
     let headers = new GetCallBackFaileResultHeaders({ });
     return await this.getCallBackFaileResultWithOptions(request, headers, runtime);
+  }
+
+  async installAppWithOptions(request: InstallAppRequest, headers: InstallAppHeaders, runtime: $Util.RuntimeOptions): Promise<InstallAppResponse> {
+    Util.validateModel(request);
+    let query : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.appId)) {
+      query["appId"] = request.appId;
+    }
+
+    if (!Util.isUnset(request.corpId)) {
+      query["corpId"] = request.corpId;
+    }
+
+    if (!Util.isUnset(request.dingUserId)) {
+      query["dingUserId"] = request.dingUserId;
+    }
+
+    if (!Util.isUnset(request.suiteId)) {
+      query["suiteId"] = request.suiteId;
+    }
+
+    let realHeaders : {[key: string ]: string} = { };
+    if (!Util.isUnset(headers.commonHeaders)) {
+      realHeaders = headers.commonHeaders;
+    }
+
+    if (!Util.isUnset(headers.xAcsDingtalkAccessToken)) {
+      realHeaders["x-acs-dingtalk-access-token"] = Util.toJSONString(headers.xAcsDingtalkAccessToken);
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      headers: realHeaders,
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "InstallApp",
+      version: "event_1.0",
+      protocol: "HTTP",
+      pathname: `/v1.0/event/elm/apps/install`,
+      method: "POST",
+      authType: "AK",
+      style: "ROA",
+      reqBodyType: "none",
+      bodyType: "json",
+    });
+    return $tea.cast<InstallAppResponse>(await this.execute(params, req, runtime), new InstallAppResponse({}));
+  }
+
+  async installApp(request: InstallAppRequest): Promise<InstallAppResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers = new InstallAppHeaders({ });
+    return await this.installAppWithOptions(request, headers, runtime);
+  }
+
+  async installCoolAppWithOptions(tmpReq: InstallCoolAppRequest, headers: InstallCoolAppHeaders, runtime: $Util.RuntimeOptions): Promise<InstallCoolAppResponse> {
+    Util.validateModel(tmpReq);
+    let request = new InstallCoolAppShrinkRequest({ });
+    OpenApiUtil.convert(tmpReq, request);
+    if (!Util.isUnset(tmpReq.feature)) {
+      request.featureShrink = OpenApiUtil.arrayToStringWithSpecifiedStyle(tmpReq.feature, "feature", "json");
+    }
+
+    if (!Util.isUnset(tmpReq.options)) {
+      request.optionsShrink = OpenApiUtil.arrayToStringWithSpecifiedStyle(tmpReq.options, "options", "json");
+    }
+
+    let query : {[key: string ]: any} = { };
+    if (!Util.isUnset(request.appId)) {
+      query["appId"] = request.appId;
+    }
+
+    if (!Util.isUnset(request.coolAppCode)) {
+      query["coolAppCode"] = request.coolAppCode;
+    }
+
+    if (!Util.isUnset(request.corpId)) {
+      query["corpId"] = request.corpId;
+    }
+
+    if (!Util.isUnset(request.featureShrink)) {
+      query["feature"] = request.featureShrink;
+    }
+
+    if (!Util.isUnset(request.installUid)) {
+      query["installUid"] = request.installUid;
+    }
+
+    if (!Util.isUnset(request.openConversationId)) {
+      query["openConversationId"] = request.openConversationId;
+    }
+
+    if (!Util.isUnset(request.optionsShrink)) {
+      query["options"] = request.optionsShrink;
+    }
+
+    if (!Util.isUnset(request.suiteId)) {
+      query["suiteId"] = request.suiteId;
+    }
+
+    let realHeaders : {[key: string ]: string} = { };
+    if (!Util.isUnset(headers.commonHeaders)) {
+      realHeaders = headers.commonHeaders;
+    }
+
+    if (!Util.isUnset(headers.xAcsDingtalkAccessToken)) {
+      realHeaders["x-acs-dingtalk-access-token"] = Util.toJSONString(headers.xAcsDingtalkAccessToken);
+    }
+
+    let req = new $OpenApi.OpenApiRequest({
+      headers: realHeaders,
+      query: OpenApiUtil.query(query),
+    });
+    let params = new $OpenApi.Params({
+      action: "InstallCoolApp",
+      version: "event_1.0",
+      protocol: "HTTP",
+      pathname: `/v1.0/event/elm/coolApps/install`,
+      method: "POST",
+      authType: "AK",
+      style: "ROA",
+      reqBodyType: "none",
+      bodyType: "json",
+    });
+    return $tea.cast<InstallCoolAppResponse>(await this.execute(params, req, runtime), new InstallCoolAppResponse({}));
+  }
+
+  async installCoolApp(request: InstallCoolAppRequest): Promise<InstallCoolAppResponse> {
+    let runtime = new $Util.RuntimeOptions({ });
+    let headers = new InstallCoolAppHeaders({ });
+    return await this.installCoolAppWithOptions(request, headers, runtime);
   }
 
   async rePushSuiteTicketWithOptions(request: RePushSuiteTicketRequest, headers: {[key: string ]: string}, runtime: $Util.RuntimeOptions): Promise<RePushSuiteTicketResponse> {
